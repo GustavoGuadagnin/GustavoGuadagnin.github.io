9b8ba7edb13a14982e294947a5f8dc6c2a573a3e
GustavoGuadagnin <107253704+GustavoGuadagnin@users.noreply.github.com>
2023-11-25 11:47:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -24,3 +24,17 @@ function redirecionar(){
    console.log(link)
    window.location.href=link;
 }
+
+const sections = document.querySelectorAll("section");
+const windowMetade = window.innerHeight * 0.6;
+function animaScroll(){
+    sections.forEach((section)=>{
+        const sectionTop = section.getBoundingClientRect().top - windowMetade;
+        console.log(sectionTop);
+        if(sectionTop<0){
+            section.classList.add("ativo")
+        }
+    })
+}
+animaScroll();
+window.addEventListener("scroll",animaScroll)
